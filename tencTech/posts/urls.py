c718d05724c5a10404a75381dc0253cd99011c92
Bhadrinath Vetry <54344138+bhadrinath95@users.noreply.github.com>
2021-03-05 15:33:09 +0530

    Add files via upload

---
+++ tencTech/posts/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, re_path
+from . import views
+
+app_name = 'posts'
+
+urlpatterns = [
+    path('', views.posts_list, name='display'),
+    path('create/', views.posts_create),
+    re_path('(?P<id>\d+)/edit/', views.posts_update, name = 'update'),
+    re_path('(?P<id>\d+)/delete/', views.posts_delete),
+    re_path('(?P<id>\d+)', views.posts_detail,name='detail'),
+    
+]
