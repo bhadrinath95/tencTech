f49067588462326770f0c34c8c3b25c183f6648a
bhadrinath95 <bhadrinath95@gmail.com>
2025-08-07 10:46:37 +0530

    Your commit message here

--- tencTech/posts/models.py
+++ tencTech/posts/models.py
@@ -8,7 +8,7 @@ from markdown_deux import markdown
 from .utils import get_read_time
 from django.contrib.contenttypes.models import ContentType
 from comments.models import Comment
-from django.urls import reverse 
+from django.urls import reverse
 
 # Create your models here.
 def upload_location(instance,filename):
@@ -18,12 +18,12 @@ def upload_location(instance,filename):
 class PostManager(models.Manager):
     def active(self,*args, **kwargs):
         return super(PostManager,self).filter(draft=False).filter(publish__lte=timezone.now())
-    
+
 class Post(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1, on_delete=models.DO_NOTHING)
     title = models.CharField(max_length=120)
     slug = models.SlugField(unique=True)
-    media_url = models.CharField(max_length=120)
+    media_url = models.CharField(max_length=120, null=True, blank=True)
     is_image = models.BooleanField(default=True)
     content = models.TextField()
     draft = models.BooleanField(default=False)
@@ -31,36 +31,36 @@ class Post(models.Model):
     read_time = models.IntegerField(default=False)
     updated = models.DateTimeField(auto_now=True,auto_now_add=False)
     timestamp = models.DateTimeField(auto_now=False,auto_now_add=True)
-    
+
     objects = PostManager()
-    
+
     def __str__(self):
         return self.title
-    
+
     def get_absolute_url(self):
         #return "/posts/%s/" %(self.id)
         return reverse("posts:detail", kwargs={"id":self.id}) #Keyword arguments
-    
+
     class Meta:
         ordering = ["-timestamp","-updated"]
-        
+
     def get_markdown(self):
         content = self.content
-        markdown_text = markdown(content)  
+        markdown_text = markdown(content)
         return mark_safe(markdown_text)
-    
+
     @property
     def comments(self):
         instance = self
         qs = Comment.objects.filter_by_instance(instance)
         return qs
-    
+
     @property
     def get_content_type(self):
         instance = self
         content_type = ContentType.objects.get_for_model(instance.__class__)
         return content_type
-    
+
 def create_slug(instance, new_slug=None):
     slug = slugify(instance.title)
     if new_slug is not None:
@@ -71,7 +71,7 @@ def create_slug(instance, new_slug=None):
         new_slug = "%s-%s" %(slug, qs.first().id)
         return create_slug(instance, new_slug = new_slug)
     return slug
-        
+
 def pre_save_post_receiver(sender, instance, *args, **kwargs):
     if not instance.slug:
         instance.slug= create_slug(instance)
@@ -79,5 +79,5 @@ def pre_save_post_receiver(sender, instance, *args, **kwargs):
         html_string = instance.get_markdown()
         read_time_var = get_read_time(html_string)
         instance.read_time = read_time_var
-    
+
 pre_save.connect(pre_save_post_receiver, sender=Post)
--- tencTech/posts/urls.py
+++ tencTech/posts/urls.py
@@ -5,7 +5,7 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', views.posts_list, name='display'),
-    path('create/', views.posts_create),
+    path('create/', views.posts_create, name = 'create'),
     re_path('(?P<id>\d+)/edit/', views.posts_update, name = 'update'),
     re_path('(?P<id>\d+)/delete/', views.posts_delete),
     re_path('(?P<id>\d+)', views.posts_detail,name='detail'),
